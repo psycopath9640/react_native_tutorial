2c15e7fc05533de524a4874dda631a090a4561d6
psycopath9640 <fatin.pantha@gmail.com>
2020-08-04 15:23:29 +0600

    react navigation P1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
-import { View } from 'react-native';
+//import { DISHES } from '../shared/dishes';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+//import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail}
+}, 
+{
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
         }
     }
+});
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId})
-    }
+class Main extends Component {
 
     render() {
         return(
-            <View style= {{flex: 1}}>
-                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />    
+            <View style= {{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MenuNavigator />
             </View>
         );
     }
