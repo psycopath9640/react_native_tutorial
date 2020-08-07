cecd8f6f45a511e9e8555f6be251148e5abc5c64
psycopath9640 <fatin.pantha@gmail.com>
2020-08-07 12:09:47 +0600

    animatable

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { baseUrl } from '../shared/baseUrl';
 import { connect } from 'react-redux';
 import { postFavorite, addComment, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -26,30 +27,32 @@ function RenderDish(props) {
 
     if (dish != null) {
         return (
-            <Card 
-                featuredTitle={dish.name}
-                image={{ uri: baseUrl + dish.image}}
-                >
-                <Text style={{margin: 10}}>
-                    {dish.description}
-                </Text>
-                <Icon
-                    raised
-                    reverse
-                    name={ props.favorite ? 'heart' : 'heart-o' }
-                    type='font-awesome'
-                    color='#f50'
-                    onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
-                    />
-                <Icon
-                    raised
-                    reverse
-                    name={'pencil'}
-                    type='font-awesome'
-                    color='#512DA8'
-                    onPress={() => props.onSelect()}
-                    />          
-            </Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card 
+                    featuredTitle={dish.name}
+                    image={{ uri: baseUrl + dish.image}}
+                    >
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                    <Icon
+                        raised
+                        reverse
+                        name={ props.favorite ? 'heart' : 'heart-o' }
+                        type='font-awesome'
+                        color='#f50'
+                        onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                        />
+                    <Icon
+                        raised
+                        reverse
+                        name={'pencil'}
+                        type='font-awesome'
+                        color='#512DA8'
+                        onPress={() => props.onSelect()}
+                        />          
+                </Card>
+            </Animatable.View>
         );
     }
     else {
@@ -72,13 +75,15 @@ function RenderComments(props) {
     }
 
     return(
-        <Card title="Comments" >
-            <FlatList 
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-                />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title="Comments" >
+                <FlatList 
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            </Card>
+        </Animatable.View>
     );
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,7 +4,7 @@ import { Tile } from 'react-native-elements';
 import { baseUrl } from '../shared/baseUrl';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
-
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -27,14 +27,16 @@ class Menu extends Component {
 
         const renderMenuItem = ({item, index}) => {
             return(
-                <Tile
-                    key={index}
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    imageSrc={{ uri: baseUrl + item.image}}
-                    />
+                <Animatable.View animation='fadeInRightBig' duration={2000}>
+                    <Tile
+                        key={index}
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        imageSrc={{ uri: baseUrl + item.image}}
+                        />
+                </Animatable.View>
             );
         }
 
