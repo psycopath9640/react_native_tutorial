323496fb4d9dbe9260766b3f536c982be8793e38
psycopath9640 <fatin.pantha@gmail.com>
2020-08-11 11:25:48 +0600

    secure storage

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import Contact from './ContactComponent';
@@ -147,6 +148,25 @@ const FavoritesNavigator = createStackNavigator({
     })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login: {screen: Login},
+}, 
+{
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        },
+        headerLeft: <Icon name='menu' size={24} 
+            color='white' 
+            onPress={() => navigation.toggleDrawer()} 
+            />
+    })
+});
+
 
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
@@ -168,6 +188,21 @@ const CustomDrawerContentComponent = (props) => (
 
 
 const MainNavigator = createDrawerNavigator({
+    Login: {
+        screen: LoginNavigator,
+        navigationOptions: {
+            title: 'Login',
+            drawLabel: 'Login',
+            drawerIcon: ({ tintColor }) => (
+                <Icon 
+                name='sign-in'
+                type='font-awesome'
+                size={24}
+                color={tintColor} 
+                />
+            )
+        }
+    },
     Home: {
         screen: HomeNavigator,
         navigationOptions: {
@@ -262,6 +297,7 @@ const MainNavigator = createDrawerNavigator({
     
 }, 
 {
+    initialRouteName: 'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
 });
